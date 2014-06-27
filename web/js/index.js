aa8e24963b4d626ef77dd9d66dc414664c00d403
Carissa Niederer <cniederer@gmail.com>
2014-06-27 13:17:26 -0500

    Added animation to shorten the intro when a playlist has successfully been made

--- web/js/index.js
+++ web/js/index.js
@@ -1,3 +1,21 @@
+// function to make jQuery support what would otherwise be .animate({height: "auto"});
+jQuery.fn.animateAuto = function(prop, speed, callback){
+    var elem, height, width;
+    return this.each(function(i, el){
+        el = jQuery(el), elem = el.clone().css({"height":"auto","width":"auto"}).appendTo("body");
+        height = elem.css("height"),
+        width = elem.css("width"),
+        elem.remove();
+        
+        if(prop === "height")
+            el.animate({"height":height}, speed, callback);
+        else if(prop === "width")
+            el.animate({"width":width}, speed, callback);  
+        else if(prop === "both")
+            el.animate({"width":width,"height":height}, speed, callback);
+    });  
+}
+
 jQuery('#save_to_spotify').click(function(e) {
 	e.preventDefault();
 	/**
@@ -28,6 +46,8 @@ jQuery('#search_form').submit(function(e) {
 			success: function(response) {
 				console.log('Success');
 				//console.log(response);
+				// jQuery('.festival-container').animate({height: "auto"});
+				jQuery(".festival-container").animateAuto("height", 500); 
 				jQuery('#save_to_spotify').fadeIn();
 				jQuery('#results').prepend(response);
 			},
