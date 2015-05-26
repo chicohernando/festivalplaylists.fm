df481da950d26d0ffb426147ce7f8f0a139d5215
Eric Sanders <easander@easander.com>
2015-05-26 01:15:06 -0500

    Added some very basic custom error messaging.  Added a .htaccess file for hosting.

--- web/index.php
+++ web/index.php
@@ -14,6 +14,18 @@
 	    'twig.path' => __DIR__ . '/../views',
 	));
 
+	$app->error(function (\Exception $e, $code) {
+	    switch ($code) {
+	        case 404:
+	            $message = 'The requested page could not be found.';
+	            break;
+	        default:
+	            $message = 'We are sorry, but something went terribly wrong.';
+	    }
+
+	    return new Response($message);
+	});
+
 	/**
 	 * Homepage
 	 */
